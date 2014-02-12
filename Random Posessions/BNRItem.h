b99bf6536308c1397e336333f090d3852cf8ae1d
Colinzel <colinzelin@gmail.com>
2014-02-12 01:53:29 -0500

    first commit

---
+++ Random Posessions/BNRItem.h
@@ -0,0 +1,13 @@
+//
+//  BNRItem.h
+//  Random Posessions
+//
+//  Created by Colin Zelin on 1/22/14.
+//  Copyright (c) 2014 Colin Zelin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+
+@end
---
+++ Random Posessions/BNRItem.m
@@ -0,0 +1,13 @@
+//
+//  BNRItem.m
+//  Random Posessions
+//
+//  Created by Colin Zelin on 1/22/14.
+//  Copyright (c) 2014 Colin Zelin. All rights reserved.
+//
+
+#import "BNRItem.h"
+
+@implementation BNRItem
+
+@end
